ab277b14166591a5a61c822573068e0687fc06b4
unknown <dini.rasyiddah1@gmail.com>
2023-07-24 23:21:06 +0700

    add recursive class

--- src/Recursive.java
+++ src/Recursive.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Recursive {
+public class Recursive {
+    public static void main(String[] args) {
+        checkData("aaaabbccdddddddddddddeeeeeeeeee");
+    }
+
+    public static void checkData(String data){
+        String[] array = data.split("(?<=(.))(?!\\1)");
+        String longestArray = recArray(array, null, 0, 0);
+        System.out.println(longestArray);
+    }
+
+    public static String recArray(String[] array, String character, int currentIndex, int longestArray){
+        if (currentIndex >= array.length){
+            return character;
+        }else{
+            int currentLength = array[currentIndex].length();
+            if (currentLength > longestArray){
+                character = String.valueOf(array[currentIndex].charAt(0));
+                longestArray = currentLength;
+            }
+            return recArray(array, character, currentIndex + 1, longestArray);
+        }
+    }
 }
